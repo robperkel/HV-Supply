ada0db0fe5e152482a1450f870e46fda779c10fc
Robert Perkel <robertp1@vt.edu>
2018-01-12 19:10:01 -0500

    Added Validation Header.

---
+++ dataValidation.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   dataValidation.h
+ * Author: Robert Perkel
+ *
+ * Uses the OpenSafety CRC8 "Standard" 
+ */
+
+#ifndef DATAVALIDATION_H
+#define	DATAVALIDATION_H
+
+#define unsigned int uint
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DATAVALIDATION_H */
+
